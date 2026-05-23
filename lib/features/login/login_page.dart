0e0b131c7ea3d52643294e58451dfbeddc665808
rianfagundes <rianfagundesv@outlook.com>
2026-05-23 14:53:10 -0300

    Classe Database Helper
    - Implementação do padrão Singleton para gerenciar a conexão com o banco de dados.
    - Criação da tabela 'usuarios' com os campos: id, nome, nome_usuario e senha.
    - Adicionados os métodos para gravar usuário e buscar dados cadastrados.
    - Configuração voltada para o ambiente offline do aplicativo em Flutter.
    - Devido a simplicidade do projeto, desconsiderei algumas medidas de prevenção, como caso o usuário digite o mesmo nome que outro usuário. Visto que, é uma probabilidade bem baixa no nosso contexto acadêmico.

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  @override
+  @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('login')),
